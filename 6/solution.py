2dc69896bf398be78a9303e5e9664120ab0042b3
Jim Willey <j2willey@gmail.com>
2024-12-05 22:40:34 -0800

    Day 6: solution 1

--- 6/solution.py
+++ 6/solution.py
@@ -4,13 +4,64 @@ import os
 script_dir = os.path.dirname(os.path.abspath(__file__))
 data_file_path = os.path.join(script_dir, 'input.txt')
 
-rules = []
-updates = []
-
+lines = None
 with open(data_file_path) as f:
     lines = f.readlines()
-    for line in lines:
-        if '|' in line:
-            rules.append([ int(i) for i in line.strip().split("|")])
-        else:
-            updates.append(line.strip().split(","))
+
+roommap = []
+for line in lines:
+    row = []
+    for l in line.strip():
+        row.append(l)
+    roommap.append(row)
+
+def printmap():
+    print("____________")
+    for row in roommap:
+        print(''.join(row))
+    print("^^^^^^^^^^^^")
+
+
+
+right = {
+    (-1, 0) : [0, 1],
+    (0, 1) : [1, 0],
+    (1, 0) : [0, -1],
+    (0, -1) : [-1, 0]
+}
+
+def findStart():
+    for i in range(len(roommap)):
+        if '^' in roommap[i]:
+            return i, roommap[i].index('^')
+
+def patrolfwd(startx, starty, dx, dy):
+    x = startx
+    y = starty
+    dp = 1   
+    m = len(roommap[0])
+    n = len(roommap)
+    while True:
+        #print(x, y, dx, dy)
+        if x < 0 or y < 0 or x >= m or y >= n:
+            break
+        if roommap[x][y] == '.' or roommap[x][y] == 'X' or roommap[x][y] == '^':
+            if roommap[x][y] == '.':
+                dp += 1
+            roommap[x][y] = 'X'
+            if 0 <= x +dx < m and 0 <= y + dy < n and roommap[x + dx][y + dy] == '#':
+                dx, dy = right[(dx, dy)]
+            x += dx
+            y += dy
+        else: # roommap[x][y] == '#':
+            print("Error: ", x, y, roommap[x][y])
+
+    return dp
+
+
+#printmap()
+startx, starty = findStart()
+dp =  patrolfwd(startx, starty, -1, 0)
+#printmap()
+# 5534
+print("distinct positions: ",dp)
